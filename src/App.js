122bb483fcf80fe4addbcd2c010f02c84554ea62
Rachel Humble <rachelhumble@me.com>
2020-05-16 23:47:51 -0400

    Starter files

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+import Header from "./components/header";
+import NavTabs from "./components/navtabs";
+import Main from "./pages/main";
+import Portfolio from "./pages/portfolio";
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+        <HashRouter>
+        <div className="body">
+            <NavTabs />
+            <Route exact path="/" component={Main} />
+            <Route exact path="/portfolio" component={Portfolio} />
+        </div>
+        </HashRouter>
+</div>
+  );
+}
+
+export default App;
