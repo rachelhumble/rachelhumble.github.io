c7547925391d8401c664b65b79d0cada291e1604
Rachel Humble <rachelhumble@me.com>
2020-06-16 23:57:07 -0400

    Portfolio update to add final project

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -11,9 +11,9 @@ function Portfolio() {
                 <div className="col">
                     <div className="card app-card">
                         <div className="card-body">
-                            <h5 className="card-title">BandiFi</h5>
-                            <p className="card-text">BandiFi is a hub for discovering events, top songs, and related artists of your favorite bands</p>
-                            <a href="https://rachelhumble.github.io/project1/" className="btn btn-primary">View</a>
+                            <h5 className="card-title">Miles4Smiles</h5>
+                            <p className="card-text">MERN application that allows runners to connect and compete in challenges to raise awareness for local businesses in their area</p>
+                            <a href="https://miles4smiles.herokuapp.com/" target="_blank" rel="noopener noreferrer" className="btn btn-primary">View</a>
                         </div>
                     </div>
                 </div>
@@ -22,7 +22,7 @@ function Portfolio() {
                         <div className="card-body">
                             <h5 className="card-title">Code Query</h5>
                             <p className="card-text">Cody Query is an application that allows bootcamp students to ask questions and answer questions from their peers in a friendly environment</p>
-                            <a href="https://code--query.herokuapp.com/" className="btn btn-primary">View</a>
+                            <a href="https://code--query.herokuapp.com/" target="_blank" rel="noopener noreferrer" className="btn btn-primary">View</a>
                         </div>
                     </div>
                 </div>
@@ -30,8 +30,8 @@ function Portfolio() {
                     <div className="card app-card">
                         <div className="card-body">
                             <h5 className="card-title">Password Generator</h5>
-                            <p className="card-text">A detailed password generator built with HTML and JavaScript; designed with CSS</p>
-                            <a href="https://rachelhumble.github.io/password_generator/" className="btn btn-primary">View</a>
+                            <p className="card-text">A detailed and functional password generator built with HTML and JavaScript; designed with CSS</p>
+                            <a href="https://rachelhumble.github.io/password_generator/" target="_blank" rel="noopener noreferrer" className="btn btn-primary">View</a>
                         </div>
                     </div>
                 </div>
@@ -41,8 +41,8 @@ function Portfolio() {
                     <div className="card app-card">
                         <div className="card-body">
                             <h5 className="card-title">Burger!</h5>
-                            <p className="card-text">A Node / Express / Handlebars burger logger application</p>
-                            <a href="https://rhumble-burger.herokuapp.com/" className="btn btn-primary">View</a>
+                            <p className="card-text"><br></br>A Node / Express / Handlebars burger logger application to demonstrate CRUD operations with MySQL</p>
+                            <a href="https://rhumble-burger.herokuapp.com/" target="_blank" rel="noopener noreferrer" className="btn btn-primary">View</a>
                         </div>
                     </div>
                 </div>
@@ -50,8 +50,8 @@ function Portfolio() {
                     <div className="card app-card">
                         <div className="card-body">
                             <h5 className="card-title">Employee Directory</h5>
-                            <p className="card-text">This REACT app pulls employee data via API and demonstrates the ability to view either all employee data or view sorted.</p>
-                            <a href="https://rachelhumble.github.io/employee-directory/" className="btn btn-primary">View</a>
+                            <p className="card-text">This React app pulls employee data via API and demonstrates the ability to view either all employee data or view sorted.</p>
+                            <a href="https://rachelhumble.github.io/employee-directory/" target="_blank" rel="noopener noreferrer" className="btn btn-primary">View</a>
                         </div>
                     </div>
                 </div>
@@ -60,7 +60,7 @@ function Portfolio() {
                         <div className="card-body">
                             <h5 className="card-title">Team Profile Generator</h5>
                             <p className="card-text">Use this CLI application to generate a browser-friendly HTML file in order to view and organize team members</p>
-                            <a href="https://github.com/rachelhumble/team-profile-generator" className="btn btn-primary">View</a>
+                            <a href="https://github.com/rachelhumble/team-profile-generator" target="_blank" rel="noopener noreferrer" className="btn btn-primary">View</a>
                         </div>
                     </div>
                 </div>
