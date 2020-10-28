733d3b49c5b7b65e15b20c34615cb6cd47198150
Rachel Humble <rachelhumble@me.com>
2020-10-27 22:28:05 -0400

    Security fixes

--- build/precache-manifest.5cc06ffd322eddaf678bf5f8b310072d.js
+++ build/precache-manifest.5cc06ffd322eddaf678bf5f8b310072d.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e194938bcfdb83f214136f2b0424bc3a",
+    "revision": "f5bd56a8ccd93ce1932009326efc7c99",
     "url": "/react-portfolio/index.html"
   },
   {
-    "revision": "d68e6564a6b00c536c34",
-    "url": "/react-portfolio/static/css/main.62c40be5.chunk.css"
+    "revision": "d900530e131c55646540",
+    "url": "/react-portfolio/static/css/main.e732630f.chunk.css"
   },
   {
     "revision": "b7999104edddc23102a8",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/js/2.ed9a122b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d68e6564a6b00c536c34",
-    "url": "/react-portfolio/static/js/main.bdb00529.chunk.js"
+    "revision": "d900530e131c55646540",
+    "url": "/react-portfolio/static/js/main.e973b418.chunk.js"
   },
   {
     "revision": "37db3f26b7c0ea36aed0",
@@ -26,6 +26,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "075e4de0fe5d733a9b058bfc578acc86",
     "url": "/react-portfolio/static/media/ceramic_background.075e4de0.jpg"
+  },
+  {
+    "revision": "144672e830be1fad80f4237e93953455",
+    "url": "/react-portfolio/static/media/ceramic_background_mobile.144672e8.jpg"
   },
   {
     "revision": "6f8a81a2a40acd02df0168fd73cd8852",
